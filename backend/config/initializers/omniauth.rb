a0621feb12966423bf1a88af310bd96c787e186a
IanTapply22 <tapplyi@students.dsbn.org>
2023-06-21 18:28:38 -0400

    Added a guard dog

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+    # You thought this was the actual one, you were wrong! This is just for testing, now get out of this territory! >:(
     provider :google_oauth2, '363347043044-gf9r1drk66uqvcmc6esqk02his5lv8jv.apps.googleusercontent.com', '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', skip_jwt: true, scope: 'email, profile'
 end
 OmniAuth.config.allowed_request_methods = %i[get]
