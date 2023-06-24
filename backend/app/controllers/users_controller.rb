6bb24a47853c7484358a5dbf2dc5740e2f12e4f0
IanTapply22 <tapplyi@students.dsbn.org>
2023-06-24 10:59:29 -0400

    Messing around with JWTs more Made it so that a user can only request their data and not everyones Removed some unneeded things when authorizing requests

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -8,6 +8,40 @@ class UsersController < ApplicationController
     private
   
     def set_user
-      @user = User.find(params[:id])
+      begin
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound # Just checking if the record isn't found, this is a janky method to do this but it works for now.
+        render json: { error: 'User not found' }, status: :not_found
+        return
+      end
+    
+      unless authorized_user?
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
+    
+    private
+    
+    def headers
+      request.headers
+    end
+    
+    def authorized_user?
+      decoded_user_id = decoded_auth_token[:user_id]
+      params[:id].to_i == decoded_user_id
+    end
+    
+    # Just manually adding this method and http_auth_header method from AuthorizeApiRequest for testing prurposes
+    def decoded_auth_token
+      @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    end
+    
+    def http_auth_header
+      if headers['Authorization'].present?
+        return headers['Authorization'].split(' ').last
+      else
+        render json: { error: 'Missing Token' }, status: 400
+      end
+      nil
+    end    
   end  
