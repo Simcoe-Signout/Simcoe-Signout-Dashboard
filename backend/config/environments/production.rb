50dffef2b6e0ef198e679e8e86189145b994dfee
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-26 23:36:23 -0400

    Reverted Updated docker start command

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -71,8 +71,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # config.hosts << "simcoe-signout-api.ian-tapply.me"
-  config.hosts.clear
+  config.hosts << "simcoe-signout-api.ian-tapply.me"
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
