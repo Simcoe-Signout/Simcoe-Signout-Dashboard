9c012e99f18c09393e5052169686755e40507500
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-27 10:53:03 -0400

    Renamed token for authorizing API requests

--- backend/app/commands/authorize_api_request.rb
+++ backend/app/commands/authorize_api_request.rb
@@ -13,7 +13,7 @@ class AuthorizeApiRequest
 
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    @user || errors.add(:token, 'Invalid token') && nil
+    @user || errors.add(:id_token, 'Invalid token') && nil
   end
 
   def decoded_auth_token
