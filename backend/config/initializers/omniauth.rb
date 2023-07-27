e87f1268daabd437666d3c8e2901daddd2b5e61a
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-27 10:20:49 -0400

    Reverted

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '363347043044-gf9r1drk66uqvcmc6esqk02his5lv8jv.apps.googleusercontent.com', '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', skip_jwt: true, scope: 'email, profile',
-    {
-        callback_path: 'https://simcoe-signout-api.ian-tapply.me/users/auth/google_oauth2/callback' 
-    }
+    provider :google_oauth2, '363347043044-gf9r1drk66uqvcmc6esqk02his5lv8jv.apps.googleusercontent.com', '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', skip_jwt: true, scope: 'email, profile', { redirect_uri: 'https://simcoe-signout-api.ian-tapply.me/users/auth/google_oauth2/callback' }
 end
 OmniAuth.config.allowed_request_methods = %i[get]
