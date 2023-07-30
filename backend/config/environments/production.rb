4e38bff7036f75417b56bb217030a71c6df90361
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-30 14:44:50 -0400

    Forcing ssl for prod environment

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.force_ssl = true
+
   config.hosts << "simcoe-signout-api.ian-tapply.me"
 
 
