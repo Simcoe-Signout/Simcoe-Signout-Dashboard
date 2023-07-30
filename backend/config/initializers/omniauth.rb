8aebf90ddf3b16d096a7f431a9aa1d69c36f5278
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-30 14:22:16 -0400

    Switched ot callback_path for omniauth again

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true, scope: 'email, profile', redirect_uri: 'https://simcoe-signout-api.ian-tapply.me/users/auth/google_oauth2/callback'
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true, scope: 'email, profile', callback_path: '/users/auth/google_oauth2/callback'
 end
 OmniAuth.config.allowed_request_methods = %i[get]
