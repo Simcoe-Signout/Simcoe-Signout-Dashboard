4840a28b9b663b9b3441a99dd58980113c749069
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-30 13:30:34 -0400

    Commented WIP stuff

--- backend/app/api/resource/v1/root.rb
+++ backend/app/api/resource/v1/root.rb
@@ -1,27 +1,27 @@
-module Resource
-    module V1
-      class Root < Grape::API
-        format :json
-        prefix 'api/resource'
-        version 'v1', using: :path
+# module Resource
+#     module V1
+#       class Root < Grape::API
+#         format :json
+#         prefix 'api/resource'
+#         version 'v1', using: :path
   
-        # Helper method to authenticate admin
-        helpers Helpers::Authentication
+#         # Helper method to authenticate admin
+#         helpers Helpers::Authentication
   
-        # Authenticate all requests
-        before do
-          authenticate_request
-          authenticate_admin
-        end
+#         # Authenticate all requests
+#         before do
+#           authenticate_request
+#           authenticate_admin
+#         end
   
-        # Mount all booking related endpoints
+#         # Mount all booking related endpoints
   
-        add_swagger_documentation(
-          hide_documentation_path: true,
-          api_version: 'v1',
-          mount_path: "/documentation",
-        )
-      end
-    end
-  end
+#         add_swagger_documentation(
+#           hide_documentation_path: true,
+#           api_version: 'v1',
+#           mount_path: "/documentation",
+#         )
+#       end
+#     end
+#   end
   
