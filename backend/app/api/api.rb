4a0ed613efddb4048856dd602fdcb9b6b6319ab2
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-30 13:33:56 -0400

    Removed more unused WIP stuff

--- backend/app/api/api.rb
+++ backend/app/api/api.rb
@@ -3,6 +3,6 @@ class Api < Grape::API
     prefix :api
     
     mount Booking::V1::Root
-    mount Resource::V1::Root
-    mount User::V1::Root
+    # mount Resource::V1::Root
+    # mount User::V1::Root
 end
