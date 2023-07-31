2072da0a5a5f3f1f69c88bce38c4457c84c3860b
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-31 18:28:46 -0400

    Updated defaults for cookie settings

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,11 +23,10 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    Rails.application.config.session_store :cookie_store, key: '_api_session', secure: true, httponly: true
-    
+
     config.api_only = true
 
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_api_session'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_api_session',  secure: true, httponly: true
   end
 end
