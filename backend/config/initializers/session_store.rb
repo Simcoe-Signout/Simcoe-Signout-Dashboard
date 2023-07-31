689b3fbe747500e6513e86fe50f19f7ce80aa0c0
IanTapply22 <tapplyi@students.dsbn.org>
2023-07-31 18:23:23 -0400

    Updated session store initializer

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,4 +1,1 @@
-Rails.application.config.before_initialize do
-    Rails.application.config.session_store :cookie_store, key: '_api_session', secure: true, httponly: true
-  end
-  
+Rails.application.config.session_store :cookie_store, key: '_api_session', secure: true, httponly: true
