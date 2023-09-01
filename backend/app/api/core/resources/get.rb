b097d0631b0715450d6e6f8d32c4557bfa2ef32b
IanTapply22 <tapplyi@students.dsbn.org>
2023-08-31 23:56:31 -0400

    Categories param now takes multiple entries

--- backend/app/api/core/resources/get.rb
+++ backend/app/api/core/resources/get.rb
@@ -2,13 +2,14 @@ module Core
   module Resources
     class Get < Grape::API
       params do
-        optional :category, type: String
+        optional :categories, type: String
       end
 
       get do
         resources = Resource.all
-        if params[:category].present?
-          resources = resources.where(category: params[:category])
+        if params[:categories].present?
+          categories = params[:categories].split(",")
+          resources = resources.where(category: categories)
         end
         resources
       end
