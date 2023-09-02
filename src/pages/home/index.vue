da8720f7451c652bd3e0e3ecd5dd632e3b14c11e
IanTapply22 <tapplyi@students.dsbn.org>
2023-09-02 14:23:27 -0400

    Added credits to homepage

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,6 @@
 <template>
     <h1 class="display-2 text-center mt-2">Welcome to Simcoe Signout!</h1>
     <h2 class="text-center color-blue font-italic">A booking platform made to allow staff to book computer devices, rooms, and resources</h2>
+    <h2 class="text-center color-blue mt-10">Made By: Ian Tapply</h2>
 </template>
   
