a4422afcd176b0c0242da24e526a9ca104c72393
Dacotah Harvey <dacotahj.harvey@gmail.com>
2023-10-20 23:34:14 -0400

    Remove selectedDates from attributes

--- src/pages/resources/components/bookingMenu.vue
+++ src/pages/resources/components/bookingMenu.vue
@@ -329,11 +329,7 @@ export default {
             const bookings = this.getBookings(resourceName);
 
             const test = [
-                ...this.selectedDates.map(date => ({
-                    highlight: true,
-                    dates: date,
-                })),
-                ...bookings
+                bookings
                     .sort((a, b) => a.period - b.period) // Sort the bookings by period value
                     .map(booking => ({
                         dates: new Date(booking.date.getFullYear(), booking.date.getMonth(), booking.date.getDate() + 1), // Shift the date forward by one day
