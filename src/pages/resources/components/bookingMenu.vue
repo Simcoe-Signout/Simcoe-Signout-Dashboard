ea67acf85f3c13e8011233aec87888d6b1d2e039
Dacotah Harvey <dacotahj.harvey@gmail.com>
2023-10-21 01:17:13 -0400

    Temporarily removed selectedDates

--- src/pages/resources/components/bookingMenu.vue
+++ src/pages/resources/components/bookingMenu.vue
@@ -331,11 +331,13 @@ export default {
             const bookings = this.getBookings(resourceName)
                                 .sort((a, b) => a.period - b.period) || [];
 
+            console.log(this.selectedDates)
+
             const test = [
-                ...this.selectedDates.map(date => ({
-                    highlight: true,
-                    dates: date,
-                })),
+                // ...this.selectedDates.map(date => ({
+                //     highlight: true,
+                //     dates: date,
+                // })),
                 ...bookings
                     .map(booking => ({
                         dates: new Date(booking.date.getFullYear(), booking.date.getMonth(), booking.date.getDate() + 1), // Shift the date forward by one day
