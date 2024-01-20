ace8c6b0f6403350bac27247933e14d8b8880e2d
IanTapply22 <contact@ian-tapply.me>
2024-01-20 11:36:48 -0500

    Fixed the issue again

--- src/pages/account/myBookings.vue
+++ src/pages/account/myBookings.vue
@@ -2,7 +2,7 @@
     <div class="ml-10">
         <h1 class="mt-5">My Bookings</h1>
         <myBookingsCalendar />
-        <deleteAllBookings v-show="!checkIfInDevelopment" />
+        <deleteAllBookings v-show="checkIfInDevelopment" />
         <v-divider class="mr-10"></v-divider>
     </div>
     <bookingItems />
