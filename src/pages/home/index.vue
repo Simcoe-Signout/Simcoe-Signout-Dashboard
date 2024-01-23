11a92782281e9cfe87ee4ce8995a4d89cd60baed
noah-nm <noah.jr.mills@gmail.com>
2024-01-23 10:11:07 -0500

    Added instructions on how to add a booking to the home page

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,5 +2,16 @@
     <h1 class="display-2 text-center mt-2">Welcome to Simcoe Signout!</h1>
     <h2 class="text-center color-blue font-italic">A booking platform made to allow staff to book computer devices, rooms, and resources</h2>
     <h2 class="text-center color-blue mt-10">Made By: Ian Tapply, Noah Mills, and Micheal DiRamio</h2>
+    <div class="text-center mt-10">
+        <h3>How to make a booking</h3>
+        <ol>
+            <li>1. Navigate to the "Resources" tab on the sidebar.</li>
+            <li>2. Click on the "book resource" button below the resource you would like to book.</li>
+            <li>3. Select the calender date you would like to have your resource booked on and select the periods you want to book your resources for below.</li>
+            <li>4. You can select multiple days and periods at the same time, just select multiple dates on the calender and select your periods in the checkbox below.</li>
+            <li>5. You can choose to select a destination and add a comment by hitting the "next" button.</li>
+            <li>6. When you are ready to finalize your booking, hit the "book" button.</li>
+        </ol>
+    </div>
 </template>
   
