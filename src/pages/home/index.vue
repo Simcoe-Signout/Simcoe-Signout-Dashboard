57b650c3265c30f3c6a0b644a0f360842d821524
noah-nm <noah.jr.mills@gmail.com>
2024-01-23 10:34:03 -0500

    fixed grammer in instructions on home page

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,10 +6,10 @@
         <h3>How to make a booking</h3>
         <ol>
             <li>1. Navigate to the "Resources" tab on the sidebar.</li>
-            <li>2. Click on the "book resource" button below the resource you would like to book.</li>
-            <li>3. Select the calender date you would like to have your resource booked on and select the periods you want to book your resources for below.</li>
-            <li>4. You can select multiple days and periods at the same time, just select multiple dates on the calender and select your periods in the checkbox below.</li>
-            <li>5. You can choose to select a destination and add a comment by hitting the "next" button.</li>
+            <li>2. Click the "book resource" button below the resource you want to book.</li>
+            <li>3. Select the calendar date you would like your resource booked on and select the periods you want to book your resources for below.</li>
+            <li>4. You can select multiple days and periods at the same time. Select multiple dates on the calendar and select your periods in the checkbox below.</li>
+            <li>5. You can select a destination and add a comment by hitting the "next" button.</li>
             <li>6. When you are ready to finalize your booking, hit the "book" button.</li>
         </ol>
     </div>
